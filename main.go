7556766fb8f83aa35858e96cc3d913a4355ff786
Jino Chun <jukoku@gmail.com>
2020-12-29 21:48:38 +0900

    Function and variables 1.2 ...string

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
-	totalLength, _ := lenAndUpper("nico")
-	fmt.Println(totalLength)
+	repeatMe("nico", "Lynn", "dal", "japan guy", "kkangttong")
 }
